8e94383b94211c36926036295683743625927379
AraaCuteUwU <71029264+AraaCuteUwU@users.noreply.github.com>
2022-07-17 23:24:32 +0800

    Add files via upload

--- src/FiraAja/AntiBlockBurn/AntiBlockBurn.php
+++ src/FiraAja/AntiBlockBurn/AntiBlockBurn.php
@@ -1,1 +1,33 @@
+<?php
 
+namespace FiraAja\AntiBlockBurn;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\block\BlockBurnEvent;
+use pocketmine\utils\Config;
+
+class AntiBlockBurn extends PluginBase implements Listener {
+	
+	/** @var Config */
+	private Config $getConfig;
+	
+	public function onEnable(): void {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->saveDefaultConfig();
+	}
+	
+	public function onBurn(BlockBurnEvent $event){
+		$player = $event->getPlayer();
+		if(empty($this->getConfig()->get("antiburn-worlds", []))){
+			$event->cancel();
+		}else{
+			foreach($this->getConfig()->getAll() as $key){
+				$from = $player->getWorld()->getFolderName();
+				if($key == $from){
+					$event->cancel();
+				}
+			}
+		}
+	}
+}
